66d6236420d19694d3158da1268907b4bcfd69b8
Shavkatov-dev <shavkatovjasurbek@gmail.com>
2026-04-30 18:44:04 +0900

    feat: MUI customized integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
+import { light } from "../scss/MaterialTheme";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log("APP COMPONENT - PAGES ROUTER");
-  return <Component {...pageProps} />;
+  const [theme] = useState(() => createTheme(light));
+  //Socket.io, Redux,
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 //app orqali global integration
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,19 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter, Roboto_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
-
-const mono = Roboto_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-});
+import { Box, Container, Stack } from "@mui/material";
+import { brown, green } from "@mui/material/colors";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Nestar Demo</title>
-        <meta name="description" content="Nestar demo project" />
-      </Head>
-
-      <div className={`${styles.page} ${inter.variable} ${mono.variable}`}>
-        <main className={styles.main}>
-          <Image src="/next.svg" alt="Next.js logo" width={100} height={20} />
-          <h1>PAGES ROUTER.</h1>
-          <h1>Hello Next 14, MIT 35 JACK</h1>
-        </main>
-      </div>
+      <Stack sx={{ background: "#81c784" }}>Header</Stack>
+      <Container>
+        <Stack flexDirection={"column"}>
+          <Box>Popular Properties</Box>
+          <Box>Top Agents</Box>
+          <Box>Top Properties</Box>
+          <Box>Events</Box>
+        </Stack>
+      </Container>
+      <Stack sx={{ background: "#a1887f" }}>Footer</Stack>
     </>
   );
 }
