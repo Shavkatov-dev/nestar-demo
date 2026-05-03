5874771d71b9e644a60c28f82ecb49fc9a47ebb0
Shavkatov-dev <shavkatovjasurbek@gmail.com>
2026-05-03 12:40:33 +0900

    feat: SASS integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,11 @@ import type { AppProps } from "next/app";
 import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 import { light } from "../scss/MaterialTheme";
 import { useState } from "react";
+import "../scss/app.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [theme] = useState(() => createTheme(light));
-  //Socket.io, Redux,
+  //Socket.io, Redux, Mui
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
