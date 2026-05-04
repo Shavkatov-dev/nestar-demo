17bfbd3b8fbc8a62a5342e0f89d102dcca76e5d4
Shavkatov-dev <shavkatovjasurbek@gmail.com>
2026-05-04 18:05:27 +0900

    feat: property list publishing

--- libs/components/homePage/TopAgentCard.tsx
+++ libs/components/homePage/TopAgentCard.tsx
@@ -1,44 +1,16 @@
-// import React from "react";
-// import { Stack, Box, Typography } from "@mui/material";
-
-// const TopAgentCard: React.FC = () => {
-//   const agentName = "MARTIN";
-//   const agentType = "AGENT";
-//   const agentImage = "IMGurl"; // Replace with the actual image URL
-
-//   return (
-//     <Stack className="top-agent-card" spacing={2}>
-//       <Box
-//         className="card-img"
-//         style={{
-//           display: "flex",
-//           justifyContent: "space-between",
-//           borderRadius: "50%",
-//           backgroundImage: `url("/img/profile/girl.svg")`,
-//           backgroundSize: "cover",
-//           backgroundPosition: "center",
-//           height: "200px", // Adjust height as needed
-//         }}
-//       >
-//         <div className="agent-name">{agentName}</div>
-//       </Box>
-
-//       <Box className="info">
-//         <strong className="title">{agentName}</strong>
-//         <Typography className="desc">{agentType}</Typography>
-//       </Box>
-//     </Stack>
-//   );
-// };
-
-// export default TopAgentCard;
-
 import React from "react";
 import { Stack, Typography } from "@mui/material";
 
 const TopAgentCard = () => {
   return (
-    <Stack sx={{ display: "flex", flexDirection: "row" }}>
+    <Stack
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        marginLeft: "2px",
+      }}
+    >
       <Stack className="top-agent-card" alignItems="center" textAlign="center">
         <img
           src="/img/profile/girl.svg"
